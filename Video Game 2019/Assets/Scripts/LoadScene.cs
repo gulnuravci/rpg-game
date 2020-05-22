8a5846ce1d0b2cd58f96065caeda18f5151a256d
gulnuravci <34399080+gulnuravci@users.noreply.github.com>
2020-05-22 05:01:29 -0400

    initial

---
+++ Video Game 2019/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public string sceneToLoad;
+    //bool objectIsActive = false;
+
+    private void Update()
+    {
+        if(gameObject.activeInHierarchy == true)
+        {
+            //gameObject.SetActive(false);
+            SceneManager.LoadScene(sceneToLoad);
+            UIFade.instance.FadeFromBlack();
+            GameManager.instance.fadingBetweenAreas = false;
+        }
+    }
+    //public string sceneName;
+
+    //private void Update()
+    //{
+
+    //}
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if(other.tag == "Player")
+    //    {
+    //        SceneManager.LoadScene(sceneName);
+    //    }
+    //}
+}
---
+++ Video Game 2019/Assets/Scripts/PickupItem.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PickupItem : MonoBehaviour
+{
+    private bool canPickUp;
+
+    public bool automaticPickUp;
+
+    public GameObject key;
+    public GameObject robot;
+    public GameObject portfolio;
+    public GameObject computer;
+    public GameObject ribbon;
+
+    bool isMainOffice = false;
+    bool isRoom220 = false;
+    bool isPrinterStation = false;
+    bool isGymComputer = false;
+    bool isGymRibbon = false;
+
+    //public GameObject questCanvas1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Main Office")
+        {
+            isMainOffice = true;
+        }
+        if (sceneName == "Room 220")
+        {
+            isRoom220 = true;
+        }
+        if(sceneName == "Printer Station" && key.activeSelf  && robot.activeSelf)
+        {
+            isPrinterStation = true;
+        }
+        if(sceneName == "Gym" && key.activeSelf && robot.activeSelf && portfolio.activeSelf)
+        {
+            isGymComputer = true;
+            isGymRibbon = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canPickUp && Input.GetButtonDown("Fire1") && PlayerController.instance.canMove && !automaticPickUp)
+        {
+            GameManager.instance.AddItem(GetComponent<Item>().itemName);
+            Destroy(gameObject);
+        }
+
+        if(canPickUp && PlayerController.instance.canMove && automaticPickUp)
+        {
+            GameManager.instance.AddItem(GetComponent<Item>().itemName);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            canPickUp = true;
+            if(isMainOffice == true)
+            {
+                key.SetActive(true);
+            }
+            if (isRoom220 == true)
+            {
+                robot.SetActive(true);
+                //GameObject.FindWithTag("Robot").SetActive(true);
+                //questCanvas1.SetActive(true);
+            }
+
+            if(isPrinterStation == true)
+            {
+                portfolio.SetActive(true);
+            }
+
+            if (isGymRibbon == true && isGymComputer)
+            {
+                computer.SetActive(true);
+                ribbon.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canPickUp = false;
+        }
+    }
+}
